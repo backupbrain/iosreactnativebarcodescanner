a6652b2cacb1e148b418f1a2fe478cbbab862b26
Tony Gaitatzis <tonyg@backupbrain.org>
2021-11-05 22:27:23 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,144 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Platform
+} from 'react-native';
+import { Camera } from 'expo-camera';
+// import { BarCodeScanner } from 'expo-barcode-camera';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [barcode, setBarcode] = useState(null);
+
+  // Screen Ratio and image padding
+  const [imagePadding, setImagePadding] = useState(0);
+  const [ratio, setRatio] = useState('4:3');  // default is 4:3
+  const { height, width } = Dimensions.get('window');
+  const screenRatio = height / width;
+  const [isRatioSet, setIsRatioSet] =  useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  const barcodeScanned = (data) => {
+    if (data.data) {
+      setBarcode(data.data)
+    } else {
+      setBarcode(null)
+    }
+  }
+  
+  // set the camera ratio and padding.
+  // this code assumes a portrait mode screen
+  const prepareRatio = async () => {
+    let desiredRatio = '4:3';  // Start with the system default
+    // This issue only affects Android
+    if (Platform.OS === 'android') {
+      const ratios = await camera.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // find the ratio that is closest to the screen ratio without going over
+      let distances = {};
+      let realRatios = {};
+      let minDistance = null;
+      for (const ratio of ratios) {
+        const parts = ratio.split(':');
+        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
+        realRatios[ratio] = realRatio;
+        // ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio; 
+        distances[ratio] = realRatio;
+        if (minDistance == null) {
+          minDistance = ratio;
+        } else {
+          if (distance >= 0 && distance < distances[minDistance]) {
+            minDistance = ratio;
+          }
+        }
+      }
+      // set the best match
+      desiredRatio = minDistance;
+      //  calculate the difference between the camera width and the screen height
+      const remainder = Math.floor(
+        (height - realRatios[desiredRatio] * width) / 2
+      );
+      // set the preview padding and preview ratio
+      setImagePadding(remainder);
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this 
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+    }
+  };
+
+  // the camera must be loaded in order to access the supported ratios
+  const setCameraReady = async() => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
+  };
+
+  if (hasCameraPermission === null) {
+    return (
+      <View style={styles.information}>
+        <Text>Waiting for camera permissions</Text>
+      </View>
+    );
+  } else if (hasCameraPermission === false) {
+    return (
+      <View style={styles.information}>
+        <Text>No access to camera</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Camera
+          style={[styles.cameraPreview, {marginTop: imagePadding, marginBottom: imagePadding}]}
+          type={type}
+          ratio={ratio}
+          ref={(ref) => {
+            setCamera(ref)
+          }}
+          onCameraReady={setCameraReady}
+          useCamera2Api
+          onBarCodeScanned={(data) => { barcodeScanned(data) }}
+        >
+          <View style={styles.buttonContainer}>
+            <Text style={styles.text}>{ barcode }</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}>
+              {type === Camera.Constants.Type.back
+                ? <Text style={styles.text}> Flip to Front Camera</Text>
+                : <Text style={styles.text}> Flip to Back Camera</Text>
+              }
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +148,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cameraPreview: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'flex-end'
+  },
+  buttonContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingBottom: 20
+  },
+  button: {
+    padding: 5
+  },
+  text: {
+    color: '#fff'
+  }
 });
---
+++ README.md
@@ -0,0 +1,94 @@
+# React Native Barcode Scanner
+
+This project was built to test the React Native Barcode scanner.
+
+This project has only been tested in iOS.
+
+In testing, I found that the default RNCamera caused compile problems in iOS, leading me to integrate the expo camera.
+
+As I was not using Expo to create the project, I had to use Expo as an integration.
+
+## Running
+
+You can run this project by checking out, initializing, and compiling
+
+```console
+$ git clone https://github.com/backupbrain/iosreactnativebarcodescanner.git
+$ cd iosreactnativebarcodescanner
+$ npm init
+$ npx react-native run-ios
+```
+
+## How this project was created
+
+### 1. Created React Native project
+
+```console
+$ npx create-react-native-app -y iosreactnativecamera
+$ cd iosreactnativecamera
+```
+
+### 2. Install Expo package 
+
+React Native doesn't support expo modules by default, so we must install them.
+
+```console
+$ npx install-expo-modules
+```
+
+More documentation available in [Installing Expo Modules](https://docs.expo.dev/bare/installing-expo-modules/)
+
+### 3. Install expo-camera and expo-barcode-scanner
+
+These modules talk to the iOS camera and the barcode scanner.
+
+Instructions for integration are available for both [expo-camera](https://github.com/expo/expo/tree/master/packages/expo-camera) and [expo-barcode-scanner](https://github.com/expo/expo/tree/master/packages/expo-barcode-scanner).
+
+```console
+$ expo install expo-camera
+$ expo install expo-barcode-scanner
+```
+
+### 4. Add permissions settings
+
+We wneed to add descriptive text to the "Camera Usage Description" (and it seems the "Microphone Usage Description") for the project. This is done by editing the `ios/iosreactnativecamera/Info.plist` file. Add the following lines inside `plist.dict`:
+
+```xml
+  <key>NSCameraUsageDescription</key>
+  <string>Allow $(PRODUCT_NAME) to use the camera</string>
+  <key>NSMicrophoneUsageDescription</key>
+  <string>Allow $(PRODUCT_NAME) to use the microphone</string>
+```
+
+### 5. Build code
+
+Since this is a simple app, we will only be editing the `App.js` file.
+
+Importantly, we will be:
+1. Importing Camera, BarcodeScanner, and other UI elements
+2. Requesting activation of the camera on first run
+3. Determining the available width/height ratio of the camera image
+4. Building the UI
+5. Displaying the barcode when the camera detects a barcode
+
+### 6. Install Cocoapods
+
+```console
+$ npx pod-install
+```
+
+### 7. Compile
+
+Compile and run. This takes a few minutes.
+
+It is important to note that the iOS Simulator doesn't support a camera.
+
+```console
+$ npx react-native run-ios
+```
+
+To test the barcode scanning and view the camera on an actual device, you must compile from Xcode by opening the project file and compiling from Xcode:
+
+```
+$ open ios/iosreactnativecamera/iosreactnativecamera.xcworkspace &
+```
